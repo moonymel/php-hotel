3d0038e308ca1c52a5f009c80c136d6c4b198c9e
moonymel <musumeci.me@gmail.com>
2024-01-25 15:41:21 +0100

    [UPDATE] added hotels list in index.php

--- index.php
+++ index.php
@@ -1,3 +1,48 @@
+<?php 
+
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+?> 
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +51,19 @@
     <title>Document</title>
     <link rel="stylesheet" href="./css/style.css">
 </head>
+
 <body>
     
+    <?php
+    foreach($hotels as $hotel){
+        echo '<h2>'.$hotel['name'].'</h2><br>';
+        echo $hotel['description'].'<br>';
+        echo $hotel['parking'].'<br>';
+        echo $hotel['vote'].'<br>';
+        echo $hotel['distance_to_center'].'<br>';
+    }
+    ?>
+
 </body>
+
 </html>
